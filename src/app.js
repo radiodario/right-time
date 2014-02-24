1520601cb7ce500de3421bbca62dc05cf59a14ef
radiodario <dario.villanueva@gmail.com>
2014-02-24 19:27:21 +0000

    i cant shaders

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-var THREE = require('./three')
-
+var THREE = require('./three');
+window.THREE = THREE;
+var Detector = require('./detector');
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, glow;
 
 var height, width, cover;
 
@@ -22,20 +23,22 @@ function init() {
 
 
 
-    camera = new THREE.PerspectiveCamera( 90, width / height, 1, 10000);
+    camera = new THREE.PerspectiveCamera( 90, width / height, 1, 1300);
     camera.position.z = width;
 
     scene = new THREE.Scene();
+    scene.add( new THREE.PointLight( 0xffffff ) );
+    scene.add( new THREE.AmbientLight( 0xffffff ) );
+
 
     geometry = new THREE.CylinderGeometry( width, width, width*2, 60, 14, true);
+    glowGeometry = new THREE.CubeGeometry( width, width, width*2, 60, 14, true);
     material = new THREE.MeshBasicMaterial( { color: 0x0088ff, wireframe: true } );
 
     // create custom material from the shader code above
     //   that is within specially labeled script tags
-    var glowMaterial = new THREE.ShaderMaterial( 
-    {
-      uniforms: 
-      { 
+    var shaderOpts = {
+      uniforms: { 
         "c":   { type: "f", value: 1.0 },
         "p":   { type: "f", value: 1.4 },
         glowColor: { type: "c", value: new THREE.Color(0xffff00) },
@@ -46,7 +49,11 @@ function init() {
       side: THREE.FrontSide,
       blending: THREE.AdditiveBlending,
       transparent: true
-    }   );
+    }  
+
+
+
+    var glowMaterial = new THREE.ShaderMaterial( shaderOpts);
 
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
@@ -56,19 +63,19 @@ function init() {
     
     mesh.position.set(- (width*0.014), -width + (width*0.03), -width/4);
     
-
+    glow = new THREE.Mesh( geometry, glowMaterial);
+    scene.add(glow);
     
-    var spriteMaterial = new THREE.SpriteMaterial( 
-      { 
-        map: new THREE.ImageUtils.loadTexture( 'images/glow.png' ), 
-        useScreenCoordinates: false, alignment: THREE.SpriteAlignment.center,
-        color: 0x0000ff, transparent: false, blending: THREE.AdditiveBlending
-      });
-    var sprite = new THREE.Sprite( spriteMaterial );
-    sprite.scale.set(width*2, width*2, 1.0);
-    mesh.add(sprite);
+    glow.position = mesh.position;
+    
+    glow.rotation.z = Math.PI/2;
+    glow.scale.multiplyScalar(1.01);
 
-    renderer = new THREE.CanvasRenderer();
+
+    // if ( Detector.webgl )
+    //   renderer = new THREE.WebGLRenderer( {antialias:true} );
+    // else
+      renderer = new THREE.CanvasRenderer(); 
     renderer.setSize( width, height );
 
     document.body.appendChild( renderer.domElement );
@@ -84,7 +91,7 @@ function animate() {
 
     mesh.rotation.x += 0.001;
     // mesh.rotation.y += 0.02;
-
+    glow.rotation.x += 0.001;
     renderer.render( scene, camera );
 
 }
